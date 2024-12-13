b17a7ea4a421da47cf5e284d4a7c8579820595d9
Yusuph-23 <165078627+Yusuph-23@users.noreply.github.com>
2024-12-13 14:06:02 +0300

    updates

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text", {
+    String:["Graphics Designer", "Motion Grapher", "2D Animator", "Photographer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+}) 
